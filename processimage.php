9709d505fb46c5bb6716e23dab0d792fbcfbc6a8
AkAsH KuMaR <34054784+Akash562@users.noreply.github.com>
2020-10-16 20:27:48 +0530

    update file name and folder

--- processimage.php
+++ processimage.php
@@ -20,7 +20,7 @@ if($uploadOk == 0){
 }else{
    /* Upload file */
    if(move_uploaded_file($_FILES['file']['tmp_name'],$location)){
-      echo $location;
+      //echo $location;
    }else{
       echo 0;
    }
